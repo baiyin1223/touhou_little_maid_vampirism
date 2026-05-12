176343c5e2ca16d06562b27495779c61ee417b12
baiyin1223 <baiyin1223@github.com>
2026-05-12 14:22:35 +0800

    v1.0.2-FIX1: Fix main hand item disappearing when vampire maid holds blood food for eating

--- src/main/java/com/github/catbert/tlmv/handler/VampireMaidTickHandler.java
+++ src/main/java/com/github/catbert/tlmv/handler/VampireMaidTickHandler.java
@@ -134,8 +134,8 @@ public class VampireMaidTickHandler {
                     }
                     // 非血瓶的血液食物走正常 startUsingItem 路径
                     ItemStack mainHandItem = maid.getMainHandItem().copy();
-                    maid.memoryHandItemStack(mainHandItem);
                     maid.setItemInHand(InteractionHand.MAIN_HAND, extracted);
+                    maid.memoryHandItemStack(mainHandItem);
                     maid.startUsingItem(InteractionHand.MAIN_HAND);
                     TLMVMain.LOGGER.debug("[VampireMaidTickHandler] Triggered feeding: {}", extracted);
                     return;
