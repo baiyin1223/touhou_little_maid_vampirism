9e95f1093b0e849f4db5aefbcc6c5590e607bc17
baiyin1223 <baiyin1223@github.com>
2026-07-02 15:47:56 +0800

    1.1.0 fix capability serialization + hunter data persistence

--- src/main/java/com/github/catbert/tlmv/capability/VampireMaidCapabilityProvider.java
+++ src/main/java/com/github/catbert/tlmv/capability/VampireMaidCapabilityProvider.java
@@ -3,13 +3,12 @@ package com.github.catbert.tlmv.capability;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import net.minecraftforge.common.util.INBTSerializable;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import net.minecraftforge.common.util.LazyOptional;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class VampireMaidCapabilityProvider implements ICapabilityProvider, INBTSerializable<CompoundTag> {
+public class VampireMaidCapabilityProvider implements ICapabilitySerializable<CompoundTag> {
     private final VampireMaidCapability capability = new VampireMaidCapability();
     private final LazyOptional<VampireMaidCapability> optional = LazyOptional.of(() -> capability);
 
--- src/main/java/com/github/catbert/tlmv/handler/FilmRestorationHandler.java
+++ src/main/java/com/github/catbert/tlmv/handler/FilmRestorationHandler.java
@@ -2,15 +2,27 @@ package com.github.catbert.tlmv.handler;
 
 import com.github.catbert.tlmv.TLMVMain;
 import com.github.catbert.tlmv.capability.ModCapabilities;
+import com.github.catbert.tlmv.network.SyncVampireMaidPacket;
+import com.github.catbert.tlmv.network.TLMVNetwork;
 import com.github.tartaricacid.touhoulittlemaid.api.event.MaidAndItemTransformEvent;
+import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
+import net.minecraft.world.entity.Entity;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.PacketDistributor;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 @Mod.EventBusSubscriber(modid = TLMVMain.MOD_ID)
 public class FilmRestorationHandler {
 
     static final String CAP_KEY = ModCapabilities.VAMPIRE_MAID_CAP_ID.toString();
+    private static final Map<UUID, CompoundTag> pendingRestore = new HashMap<>();
 
     @SubscribeEvent
     public static void onFilmToMaid(MaidAndItemTransformEvent.ToMaid event) {
@@ -26,10 +38,19 @@ public class FilmRestorationHandler {
         }
 
         CompoundTag capData = forgeCaps.getCompound(CAP_KEY);
-        ModCapabilities.getVampireMaid(event.getMaid()).ifPresent(cap -> {
+        ModCapabilities.getVampireMaid(event.getMaid()).ifPresent(cap -> cap.deserializeNBT(capData));
+    }
+
+    @SubscribeEvent
+    public static void onEntityJoinWorld(EntityJoinLevelEvent event) {
+        if (!(event.getEntity() instanceof EntityMaid maid)) return;
+        CompoundTag capData = pendingRestore.remove(maid.getUUID());
+        if (capData == null) return;
+        ModCapabilities.getVampireMaid(maid).ifPresent(cap -> {
             cap.deserializeNBT(capData);
-            TLMVMain.LOGGER.debug("Restored vampire maid capability from film: isVampire={}, level={}",
-                    cap.isVampire(), cap.getVampireLevel());
+            Entity entity = maid;
+            TLMVNetwork.INSTANCE.send(PacketDistributor.TRACKING_ENTITY.with(() -> entity),
+                    new SyncVampireMaidPacket(entity.getId(), cap.isVampire(), cap.getVampireLevel(), cap.isHunter(), cap.getHunterLevel()));
         });
     }
 
@@ -48,8 +69,8 @@ public class FilmRestorationHandler {
         CompoundTag capData = forgeCaps.getCompound(CAP_KEY);
         ModCapabilities.getVampireMaid(maid).ifPresent(cap -> {
             cap.deserializeNBT(capData);
-            TLMVMain.LOGGER.debug("Restored vampire maid capability from film: isVampire={}, level={}",
-                    cap.isVampire(), cap.getVampireLevel());
+            TLMVNetwork.INSTANCE.send(PacketDistributor.TRACKING_ENTITY.with(() -> maid),
+                    new SyncVampireMaidPacket(maid.getId(), cap.isVampire(), cap.getVampireLevel(), cap.isHunter(), cap.getHunterLevel()));
         });
     }
 }
