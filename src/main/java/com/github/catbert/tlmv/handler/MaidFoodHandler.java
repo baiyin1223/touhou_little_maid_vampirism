c44613d5f116484ad23dc92df9a9f0ee79f25619
baiyin1223 <baiyin1223@github.com>
2026-07-03 16:10:28 +0800

    fix: feeding weapon swap & double heart consumption

--- src/main/java/com/github/catbert/tlmv/handler/MaidFoodHandler.java
+++ src/main/java/com/github/catbert/tlmv/handler/MaidFoodHandler.java
@@ -113,9 +113,8 @@ public class MaidFoodHandler {
                             restoreAmount = bloodToConsume;
                             TLMVMain.LOGGER.debug("[MaidFoodHandler] Blood bottle consumed: amount={}, damage={}/{}",
                                     bloodToConsume, result.getDamageValue(), result.getMaxDamage());
-                        } else {
-                            result.shrink(1);
                         }
+                        // 非血瓶物品：eat() 已 shrink(1)，无需额外消耗
                         event.setResultStack(result);
                     }
 
